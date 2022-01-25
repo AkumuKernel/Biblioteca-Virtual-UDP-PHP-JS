b40327db382b19a7d70c6dfc148e5951b3997d3d
AkumuKernel <50686824+AkumuKernel@users.noreply.github.com>
2022-01-25 13:21:17 -0300

    Delete app2.js

--- js/app2.js
+++
@@ -1,9 +0,0 @@
- setTimeout(function(){
-   console.log("Loaded");
-
-   document.querySelector("#close").onclick = function(){
-     $.get( "../api.php/usuario/logout", function( data ) {
-       window.location.href = `index.html?msg=${data.msg}`; 
-});
-   }
- }, 1000);
